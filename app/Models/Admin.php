d91d0160cae52fda941735f8648d251e65a979d3
younessbenssassi <benssassi99@gmail.com>
2022-06-13 21:34:53 +0100

    Normal commit

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,11 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Admin extends Model
+class Admin extends Authenticatable implements JWTSubject
 {
     use HasFactory;
-    public function todos(){
-        return $this->morphToMany(Todo::class,'todoable');
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims(): array
+    {
+        return [];
     }
 }
